f530a432a7826646a101d6928f4ab39ed047ace0
admin.ian.swerts <iansw.stank@gmail.com>
2025-03-28 11:17:21 +0000

    start to implement the play and restart methods on click on play and restart buttons

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { Crosshair } from './input/crosshair.js';
 import { Duck } from './game/duck.js';
 
+import { startGame, restartGame } from './game/game.js'
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM loaded');
     const crosshair = Crosshair();
@@ -9,8 +11,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const duck = Duck();
     const spawnData = duck.spawn(gameArea);
-    duck.move(spawnData.duckElement,spawnData.movement,spawnData.initialPosition)
+    duck.move(spawnData.duckElement, spawnData.movement, spawnData.initialPosition)
 
     // Add this debug line
     console.log('Game area HTML after duck:', gameArea.innerHTML);
 });
+
+//doesnt exist you will adapt this 
+const startButton = document.querySelector('.playbtn');
+const restartButton = document.querySelector('.restartbtn');
+
+
+startButton.addEventListener('click', () => {
+    console.log("start game clicked");
+    startGame();
+});
+
+restartButton.addEventListener('click', () => {
+    console.log("restart game clicked");
+    restartGame();
+})
